f057a5576ddaabed215088afbcff7164896c5610
syu99 <ufcmjohan@gmail.com>
2022-03-19 13:10:18 +0100

    project finish

--- index.js
+++ index.js
@@ -4,4 +4,17 @@
 
 // Stocker la valeur du précédent niveau de scroll pour savoir si l'on est monté ou descendu
 
-// Connaitre niveau de scroll (window.scrollY)
+// Connaitre niveau de scroll ( )
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY < lastScroll) {
+        navbar.style.top = 0;
+    } else {
+        navbar.style.top = "-60px";
+    }
+
+    lastScroll = window.scrollY;
+})
